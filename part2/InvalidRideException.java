c33771fe6c4312861c3c296660bfafa4d5c095f8
Guilherme dos Santos <gs92dev@gmail.com>
2023-07-10 10:39:07 -0500

    exclude part 2

--- part2/InvalidRideException.java
+++
@@ -1,7 +0,0 @@
-/**
- * Throw this exception class in TaxiService.validateRide(Ride) method 
- * when the ride is not valid. There is nothing required to do in this class.
- */
-public class InvalidRideException extends Exception{
-
-}
